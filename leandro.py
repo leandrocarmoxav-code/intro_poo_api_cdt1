b83e479c19ac82795f4545579dd25b9ce1d17150
leandrocarmoxav-code <leandrocarmoxav@gmail.com>
2026-09-02 17:03:34 -0300

    upgrade

---
+++ leandro.py
@@ -0,0 +1,32 @@
+'''
+
+
+
+
+
+
+
+
+
+
+'''
+
+
+import requests
+
+url_api = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL"
+
+resposta = requests.get(url_api)
+
+if resposta.status_code == 200:
+    dados = resposta.json()
+    print("Conexão estabelecida com sucesso!")
+else:
+    print(f"Erro na requisicão: {resposta.status_code}")
+    
+    cotacao_dolar = dados ["USDBRL"]["bid"]
+    nome_moeda = dados ["USDBRL"]["name"]
+    
+    print(f"Moeda: {nome_moeda}")
+    print(f"Valor Atual de compra: R$ {float(cotacao_dolar):.2f}")
+    
